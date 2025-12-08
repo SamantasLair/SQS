07071d9e9e58e87a184c2d233cc9a6b2e24e1898
Fiiboy7 <ramadhanirofik@gmail.com>
2025-12-08 22:32:59 +0700

    Commit

--- database/migrations/2025_09_15_140103_create_questions_table.php
+++ database/migrations/2025_09_15_140103_create_questions_table.php
@@ -6,23 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('questions', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('quiz_id')->constrained()->onDelete('cascade');
-            $table->text('question_text'); 
-            $table->string('question_type'); 
+            $table->uuid('id')->primary();
+            $table->foreignUuid('quiz_id')->constrained()->onDelete('cascade');
+            $table->text('question_text');
+            $table->string('question_type')->default('multiple_choice');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('questions');
